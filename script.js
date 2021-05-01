88d02a6b0ddc15be17ae904048406aed8c92e350
Andrews-repo <andrewpatty3@gmail.com>
2021-04-30 20:33:49 -0700

    notes

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 const gridContainer = document.querySelector("#grid-container");
 const resetButton = document.querySelector("#reset-button");
 
+//event listeners for loading/changing the grid
 window.addEventListener("load", setDefaultGrid);
 resetButton.addEventListener("click", changeSize);
 
+//initial grid that loads
 function setDefaultGrid() {
   setGridSize(16);
   fillGrid(16);
 }
 
+//uses "fr" fractional sizing to set grid size
 function setGridSize(size) {
   gridContainer.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 }
 
+//creates grid elements and fills in the main container
 function fillGrid(size) {
   for (let i = 0; i < size * size; i++) {
     const gridElement = document.createElement("div");
@@ -22,6 +26,7 @@ function fillGrid(size) {
   }
 }
 
+//random background color generation
 function changeColor(e) {
   const randomR = Math.floor(Math.random() * 256);
   const randomG = Math.floor(Math.random() * 256);
@@ -29,6 +34,7 @@ function changeColor(e) {
   e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
 }
 
+//allows custom size options
 function changeSize() {
   let newSize = prompt("Enter new size");
 
@@ -45,6 +51,7 @@ function changeSize() {
   }
 }
 
+//resets grid for new size
 function clearGrid() {
   const gridArray = Array.from(gridContainer.childNodes);
   gridArray.forEach((element) => {
